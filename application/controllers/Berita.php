cb827491d5100788fd2dcfceea45a79a2fd8d8d7
Nikki Rufiansya <nikkirufiansya88@gmail.com>
2018-12-10 08:46:22 +0700

    Update Berita.php

--- application/controllers/Berita.php
+++ application/controllers/Berita.php
@@ -4,32 +4,38 @@
  */
 class Berita extends CI_Controller
 {
-	
-	function __construct()
-	{
-		parent::__construct();
+    
+    function __construct()
+    {
+        parent::__construct();
         $this->load->library('curl');
-        //$this->load->helper('form');
-	}
+        $this->load->helper('form');
+        //$this->load->helper('url');
+    }
 
-	function index(){
+    function index(){
         $this->load->view('form_berita1');
     }
 
     function masukan_Berita(){
-    	$berita = $this->input->post('isi_berita');
-    	$data = array('text' => $berita);
-    	$url = 'https://api.prosa.ai/v1/topics';
+        $berita = $this->input->post('isi_berita');
+        $data = array('text' => $berita);
+        $url = 'https://api.prosa.ai/v1/topics';
         $urlHates = 'https://api.prosa.ai/v1/hates';
+        $urlSentiments = 'https://api.prosa.ai/v1/sentiments';
         
         $display =  $this->curl->postCURL($url,$data);
         $displayHates = $this->curl->postCURL($urlHates,$data);
+        $displaySentiments = $this->curl->postCURL($urlSentiments, $data);
         $json_object = json_decode($display);
         $json_objectHates = json_decode($displayHates);
+        $json_objectSentiments = json_decode($displaySentiments);
         $topics = $json_object->topic;
         $confidence = $json_object->confidence;
         $hate_speech = $json_objectHates->hate_speech;
         $confidenceHates = $json_objectHates->confidence;
+        $sentiments = $json_objectSentiments->polarity;
+        $confidenceSentiments = $json_objectSentiments->confidence;
         echo "News : " .$berita. "<br><br>";
         echo "Topics : " .$topics. "<br><br>";
         echo "Confidence : " .$confidence."<br><br>";
@@ -38,7 +44,9 @@ class Berita extends CI_Controller
         }elseif ($hate_speech == false) {
             echo "hate speech : false<br><br>";
         }
-        echo "confidence hates : ".$confidenceHates;
+        echo "confidence hates : ".$confidenceHates."<br><br>";
+        echo "sentiments : " .$sentiments."<br><br>";
+        echo "confidence sentiments : " .$confidenceSentiments."<br><br>";
 
 
        
