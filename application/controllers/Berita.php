97fa5143a8fba19b9b37ad9bd2dc05113771f69c
Jumawal <jumawalgbk@gmail.com>
2018-12-17 06:12:45 +0700

    upload

--- application/controllers/Berita.php
+++ application/controllers/Berita.php
@@ -10,11 +10,11 @@ class Berita extends CI_Controller
         parent::__construct();
         $this->load->library('curl');
         $this->load->helper('form');
-        //$this->load->helper('url');
+        $this->load->helper('url');
     }
 
     function index(){
-        $this->load->view('form_berita1');
+        $this->load->view('form_berita');
     }
 
     function masukan_Berita(){
@@ -22,20 +22,31 @@ class Berita extends CI_Controller
         $data = array('text' => $berita);
         $url = 'https://api.prosa.ai/v1/topics';
         $urlHates = 'https://api.prosa.ai/v1/hates';
-        $urlSentiments = 'https://api.prosa.ai/v1/sentiments';
-        
+        //$urlSentiments = 'https://api.prosa.ai/v1/sentiments';
+        $urlNormals = 'https://api.prosa.ai/v1/normals';
+
+
         $display =  $this->curl->postCURL($url,$data);
         $displayHates = $this->curl->postCURL($urlHates,$data);
-        $displaySentiments = $this->curl->postCURL($urlSentiments, $data);
+        //$displaySentiments = $this->curl->postCURL($urlSentiments, $data);
+        $displayNormals = $this->curl->postCURL($urlNormals,$data);
+
         $json_object = json_decode($display);
         $json_objectHates = json_decode($displayHates);
-        $json_objectSentiments = json_decode($displaySentiments);
+        //$json_objectSentiments = json_decode($displaySentiments);
+        $json_objectNormals = json_decode($displayNormals);
+
+
         $topics = $json_object->topic;
         $confidence = $json_object->confidence;
         $hate_speech = $json_objectHates->hate_speech;
         $confidenceHates = $json_objectHates->confidence;
-        $sentiments = $json_objectSentiments->polarity;
-        $confidenceSentiments = $json_objectSentiments->confidence;
+        // $sentiments = $json_objectSentiments->polarity;
+        // $confidenceSentiments = $json_objectSentiments->confidence;
+        $normals = $json_objectNormals->text;
+
+
+
         echo "News : " .$berita. "<br><br>";
         echo "Topics : " .$topics. "<br><br>";
         echo "Confidence : " .$confidence."<br><br>";
@@ -45,8 +56,9 @@ class Berita extends CI_Controller
             echo "hate speech : false<br><br>";
         }
         echo "confidence hates : ".$confidenceHates."<br><br>";
-        echo "sentiments : " .$sentiments."<br><br>";
-        echo "confidence sentiments : " .$confidenceSentiments."<br><br>";
+        // echo "sentiments : " .$sentiments."<br><br>";
+        // echo "confidence sentiments : " .$confidenceSentiments."<br><br>";
+        echo "Word Normalizer : " .$normals;
 
 
        
